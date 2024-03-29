61a2477b1f2794ff227ea2c4c51c507b5a7c800b
WLOT <vladlotov@ukr.net>
2024-03-29 21:55:28 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+from parseSetUp import parse_arguments
+from syncApp import *
+
+def main():
+    #retrieve console output as a set of inctructions
+    args = parse_arguments()
+    setup_logging(args.l)
+
+    while True:
+        actions = compare_folders(args.s, args.r)
+        synchronize_folders(actions)
+        time.sleep(args.i)
+
+if __name__ == "__main__":
+    main()
