6d4b6d75268ad7334e2ab7a240345050fae68977
WLOT <vladlotov@ukr.net>
2024-04-01 16:40:04 +0200

    user friendly command line

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ def main():
     #retrieve console output as a set of inctructions
     args = parse_arguments()
     setup_logging(args.l)
-
     while True:
         actions = compare_folders(args.s, args.r)
         synchronize_folders(actions)
+        print(f"Folder synchronization interval is set to {args.i} seconds. If you want to stop " \
+                "press Ctrl+C - otherwise the app will restart automatically")         
         time.sleep(args.i)
 
 if __name__ == "__main__":
