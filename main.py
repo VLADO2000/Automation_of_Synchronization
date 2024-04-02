aa2e947dd6b0d1866477c2495086a86bfadc37d6
WLOT <vladlotov@ukr.net>
2024-04-02 11:40:55 +0200

    syncornization time interval

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ def main():
     args = parse_arguments()
     setup_logging(args.l)
     while True:
+        start_time = time.time()
         actions = compare_folders(args.s, args.r)
         synchronize_folders(actions)
+        end_time = time.time()
+        eclipsed_time = end_time - start_time
+        print(f'Folder synchronization took {eclipsed_time:.6f} seconds.')
         print(f"Folder synchronization interval is set to {args.i} seconds. If you want to stop " \
                 "press Ctrl+C - otherwise the app will restart automatically")         
         time.sleep(args.i)
